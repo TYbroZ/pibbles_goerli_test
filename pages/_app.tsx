f448cfa79f111a6ede68333b47bffd90e7c1557f
TYbroZ <thebluemountains@gmail.com>
2023-06-23 22:05:21 -0700

    navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-
+import {Navbar} from "../components/Navbar";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
